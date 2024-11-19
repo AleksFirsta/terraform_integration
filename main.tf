7c7ee6cfd19cb59c37fbfde7594843761e4cd5eb
aleks <grigorenko@hpe.com>
2024-11-19 18:10:14 +0200

    add vpc

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-morpheus-int"
+    key    = "terraform/main.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
