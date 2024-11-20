84e3d05a466eca963fdef9cbeccb8607784e8d61
aleks <grigorenko@hpe.com>
2024-11-20 09:44:23 +0200

    add

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
-  #   backend "s3" {
-  #     bucket = "terraform-morpheus-int"
-  #     key    = "terraform/main.tfstate"
-  #     region = "eu-north-1"
-  #   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 resource "aws_vpc" "example" {
