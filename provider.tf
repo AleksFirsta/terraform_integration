83ca0bfb080ba7c89e1bc2186fd193e86af4f676
aleks <grigorenko@hpe.com>
2024-11-20 09:39:51 +0200

    add

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region     = "eu-north-1"
-  # access_key = var.access_key
-  # secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
   #profile = "root"
 }
