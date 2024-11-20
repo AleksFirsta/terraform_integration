2e49336690617c6b6d0c2402038c42a8a61dceaf
aleks <grigorenko@hpe.com>
2024-11-20 09:46:25 +0200

    add

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
